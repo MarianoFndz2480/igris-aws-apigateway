ed3edddbf8535afac5584e4f333d48bb44f74008
Mariano Fernández <marianofndz2480@gmail.com>
2024-09-01 23:17:54 -0300

    feat: upgrade igris-core package

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,1 @@
-import { RequestAdapter, RequestProcessor } from 'igris-core'
-import { AWSApiGatewayRequestAdapter } from './event-adapter'
-
-export class AWSApiGatewayRequestProcessor extends RequestProcessor {
-    static getRequestAdapter(): RequestAdapter {
-        return new AWSApiGatewayRequestAdapter()
-    }
-}
+export * from './event-adapter'
